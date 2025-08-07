89be49e04d48177c2d8083870c90c717625172eb
Bamz Kittisak Udomsri <76609512+brownyroll@users.noreply.github.com>
2025-08-08 00:23:21 +0700

    init team

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,60 +3,60 @@ import { defineConfig } from "astro/config";
 import starlight from "@astrojs/starlight";
 import tailwindcss from "@tailwindcss/vite";
 
+import react from "@astrojs/react";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    starlight({
-      title: "Ratree Wiki",
-      description: "wiki for user play ratree server",
-      // defaultLocale: 'en',
-      // locales: {
-      // 	en: {
-      // 	label: 'English',
-      // 	},
-      // 	'zh-cn': {
-      // 	label: '简体中文',
-      // 	lang: 'zh-CN',
-      // 	},
-      // 	// Arabic docs in `src/content/docs/ar/`
-      // 	ar: {
-      // 	label: 'العربية',
-      // 	dir: 'rtl',
-      // 	},
-      // },
-      social: [
-        {
-          icon: "github",
-          label: "GitHub",
-          href: "https://github.com/ratree-minecraft",
-        },
+  integrations: [starlight({
+    title: "Ratree Wiki",
+    description: "wiki for user play ratree server",
+    // defaultLocale: 'en',
+    // locales: {
+    // 	en: {
+    // 	label: 'English',
+    // 	},
+    // 	'zh-cn': {
+    // 	label: '简体中文',
+    // 	lang: 'zh-CN',
+    // 	},
+    // 	// Arabic docs in `src/content/docs/ar/`
+    // 	ar: {
+    // 	label: 'العربية',
+    // 	dir: 'rtl',
+    // 	},
+    // },
+    social: [
+      {
+        icon: "github",
+        label: "GitHub",
+        href: "https://github.com/ratree-minecraft",
+      },
+    ],
+    sidebar: [
+      
+      {
+        label: "Getting Started",
+        items: [
+          { label: "How to join", slug: "wiki/how-to-join" },
+          { label: "How to buy", slug: "wiki/how-to-shop" }
+          
       ],
-      sidebar: [
-        
-        {
-          label: "Getting Started",
-          items: [
-            { label: "How to join", slug: "wiki/how-to-join" },
-            { label: "How to buy", slug: "wiki/how-to-shop" }
-            
-        ],
-        },
-        {
-          label: "Guides",
-          autogenerate: { directory: "wiki/guides" },
+      },
+      {
+        label: "Guides",
+        autogenerate: { directory: "wiki/guides" },
 
-        },
-        {
-          label: "Skills",
-          autogenerate: { directory: "wiki/skills" },
-        },
-        {
-          label: "Examples",
-          autogenerate: { directory: "wiki/example" },
-        },
-      ],
-    }),
-  ],
+      },
+      {
+        label: "Skills",
+        autogenerate: { directory: "wiki/skills" },
+      },
+      {
+        label: "Examples",
+        autogenerate: { directory: "wiki/example" },
+      },
+    ],
+  }), react()],
 
   vite: {
     plugins: [tailwindcss()],
